b5e2b389bad62ec880240b425e22f455547cda4e
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-11-28 05:44:01 +0200

    added realtime fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,15 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  // const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
  
 
   // const posts = [
@@ -46,6 +48,7 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
