277274a489c7d0e51f804f1c58491724c65ddac3
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-11-28 05:30:12 +0200

    defined card types and removed the error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -10,9 +10,7 @@ export default async function Home({ searchParams }: {
   const query = (await searchParams).query;
 
   const posts = await client.fetch(STARTUPS_QUERY)
-  console.log('====================================');
-  console.log(JSON.stringify(posts, null, 2));
-  console.log('====================================');
+ 
 
   // const posts = [
   //   {
@@ -40,7 +38,7 @@ export default async function Home({ searchParams }: {
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard, index: number) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
